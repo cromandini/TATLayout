acda13e153a08f483649d1c38ff3afd1a580dc56
Claudio Romandini <cromandini@me.com>
2014-03-14 19:59:52 -0300

    Add UIView+TATLayoutHierarchy spec and implementation

---
+++ TATLayout/UIView+TATLayoutHierarchy.h
@@ -0,0 +1,31 @@
+//
+//  UIView+TATLayoutHierarchy.h
+//  TATLayout
+//
+
+@import UIKit;
+
+/**
+ This category in `UIView` provides support for inspecting the view hierarchy to see if a view is ancestor of another and to calculate the closest ancestor shared by 2 views.
+ */
+@interface UIView (TATLayoutHierarchy)
+
+///------------------------------------
+/// @name Inspecting the View Hierarchy
+///------------------------------------
+
+/**
+ Returns a Boolean value that indicates whether the receiver is ancestor of a given view (being a view ancestor of itself).
+ @param view The view to test if it is within scope of the receiver.
+ @return `YES` if `view` is within scope of the receiver, otherwise `NO`. If `view` is identical to the receiver, returns `YES`.
+ */
+- (BOOL)tat_isAncestorOfView:(UIView *)view; // TODO: Should this method be private?
+
+/**
+ Returns the closest ancestor shared by the receiver and a given view.
+ @param view The view to test (along with the receiver) for closest shared ancestor.
+ @return The closest shared ancestor or `nil` if there’s no such object. Returns `self` if `view` is identical to the receiver.
+ */
+- (UIView *)tat_closestAncestorSharedWithView:(UIView *)view;
+
+@end
---
+++ TATLayout/UIView+TATLayoutHierarchy.m
@@ -0,0 +1,84 @@
+//
+//  UIView+TATLayoutHierarchy.m
+//  TATLayout
+//
+
+#import "UIView+TATLayoutHierarchy.h"
+
+@implementation UIView (TATLayoutHierarchy)
+
+#pragma mark - Inspecting the View Hierarchy
+
+- (BOOL)tat_isAncestorOfView:(UIView *)view
+{
+    if (!view) {
+        return NO;
+    }
+    if (view == self) {
+        return YES;
+    }
+    __block BOOL isAncestor = NO;
+    [view tat_enumerateSuperviewsUsingBlock:^(UIView *superview, BOOL *stop) {
+        if (superview == self) {
+            isAncestor = YES;
+            *stop = YES;
+        }
+    }];
+    return isAncestor;
+}
+
+- (UIView *)tat_closestAncestorSharedWithView:(UIView *)view
+{
+    if (!view) {
+        return nil;
+    }
+    if ([self tat_isAncestorOfView:view]) {
+        return self;
+    }
+    __block UIView *ancestor = nil;
+    [self tat_enumerateSuperviewsUsingBlock:^(UIView *superview, BOOL *stop) {
+        if ([superview tat_isAncestorOfView:view]) {
+            ancestor = superview;
+            *stop = YES;
+        }
+    }];
+    return ancestor;
+}
+
+/**
+ Enumerates the receiver's superviews using the given block, starting with `self.superview` and continuing upwards in the hierarchy.
+ @param block The block to apply to superviews. The block takes two arguments, `superview`: The superview enumerated. `stop`: A reference to a Boolean value that the block can set to YES to stop further enumeration.
+ */
+- (void)tat_enumerateSuperviewsUsingBlock:(void (^)(UIView *superview, BOOL *stop))block
+{
+    BOOL stop = NO;
+    UIView *superview = self.superview;
+    while (superview) {
+        block(superview, &stop);
+        if (stop) {
+            return;
+        }
+        superview = superview.superview;
+    }
+}
+
+/**
+ Enumerates the receiver's related constraints (ie:constraits with the receiver as the first item) using the given block, starting with `self.constraints` and continuing with `superview.constraints` and upwards in the hierarchy.
+ @param block The block to apply to related constraints. The block takes one argument, `constraint`: The related constraint enumerated.
+ */
+- (void)tat_enumerateRelatedConstraintsUsingBlock:(void (^)(NSLayoutConstraint *constraint))block // TODO: is this needed?
+{
+    void (^enumerateConstraints)(NSArray *constraints) = ^(NSArray *constraints) {
+        for (NSLayoutConstraint *constraint in constraints) {
+            if (constraint.firstItem == self) {
+                block(constraint);
+            }
+        }
+    };
+    enumerateConstraints(self.constraints);
+    [self tat_enumerateSuperviewsUsingBlock:^(UIView *superview, BOOL *stop) {
+        enumerateConstraints(superview.constraints);
+    }];
+}
+
+@end
