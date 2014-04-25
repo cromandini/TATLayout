5f235e57cfe25a052006c00a03e9d1e83825bcf9
Claudio Romandini <cromandini@me.com>
2014-04-25 12:19:54 -0300

    More renaming

--- TATLayout/NSLayoutConstraint+TATConstraintFactory.h
+++ TATLayout/NSLayoutConstraint+TATConstraintFactory.h
@@ -6,7 +6,7 @@
 @import UIKit;
 
 /**
- This category in `NSLayoutConstraint` provides support for creating constraints with a linear equation format string. The format string describes a linear equation with the addition of the priority and can be of 2 types, depending on the number of views related:
+ This category in `NSLayoutConstraint` provides support for creating constraints with an equation format string. The format string describes a linear equation with the addition of the priority and can be of 2 types, depending on the number of views related:
  
     item1.attribute1 == item2.attribute2 * multiplier + constant @priority
     item1.attribute1 == constant @priority
@@ -41,7 +41,7 @@
     otherView.top == 500 @251
     otherView.left == 0
  
- `superview` is a keyword which is set to be the superview of the first item. That said, when using `superview` as second item, the first item must be added to its superview before creating the constraint. Examples:
+ `superview` is a keyword which is set to be the superview of the first item. That said, when using `superview` as second item, the first item must be added to its superview before creating the constraint, because the view hierarchy must be inspected in order to get a reference to the superview. Examples:
  
     otherView.bottom == superview.bottom - someMetricName
     otherView.right <= superview.right * someMetricName - otherMetricName @yetAnotherMetricName
@@ -82,7 +82,7 @@
 ///----------------------------------------------------
 
 /**
- Creates a constraint explicitly, described by a linear equation format string.
+ Creates a constraint explicitly, described by an equation format string.
  @param format The format specification for the constraint.
  @param metrics A dictionary of constants that appear in the equation format string. The keys must be the string values used in the equation format string, and the values must be `NSNumber` objects.
  @param views A dictionary of views that appear in the equation format string. The keys must be the string values used in the equation format string, and the values must be the view objects.
