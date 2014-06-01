5f601b1e876d343f206a87e5862d5294840d6e46
Claudio Romandini <cromandini@me.com>
2014-06-01 14:23:25 -0300

    Use context instead of describe where appropriate

--- TATLayoutTests/UIView+TATHierarchySpec.m
+++ TATLayoutTests/UIView+TATHierarchySpec.m
@@ -14,7 +14,7 @@ describe(@"View Hierarchy", ^{
     TATFakeViewHierarchy *vh = [TATFakeViewHierarchy new];
 	
     describe(@"ancestor of view", ^{
-        describe(@"nil", ^{
+        context(@"nil", ^{
             it(@"is not ancestor of any view", ^{
                 [[theValue([vh.view1 tat_isAncestorOfView:nil]) should] beNo];
                 [[theValue([vh.view2 tat_isAncestorOfView:nil]) should] beNo];
@@ -25,7 +25,7 @@ describe(@"View Hierarchy", ^{
                 [[theValue([vh.view7 tat_isAncestorOfView:nil]) should] beNo];
             });
         });
-        describe(@"every view", ^{
+        context(@"every view", ^{
             it(@"is ancestor of itself", ^{
                 [[theValue([vh.view1 tat_isAncestorOfView:vh.view1]) should] beYes];
                 [[theValue([vh.view2 tat_isAncestorOfView:vh.view2]) should] beYes];
@@ -33,7 +33,7 @@ describe(@"View Hierarchy", ^{
                 [[theValue([vh.view7 tat_isAncestorOfView:vh.view7]) should] beYes];
             });
         });
-        describe(@"view1", ^{
+        context(@"view1", ^{
             it(@"is ancestor of views 2, 3, 4, 5 and 6", ^{
                 [[theValue([vh.view1 tat_isAncestorOfView:vh.view2]) should] beYes];
                 [[theValue([vh.view1 tat_isAncestorOfView:vh.view3]) should] beYes];
@@ -45,7 +45,7 @@ describe(@"View Hierarchy", ^{
                 [[theValue([vh.view1 tat_isAncestorOfView:vh.view7]) should] beNo];
             });
         });
-        describe(@"view2", ^{
+        context(@"view2", ^{
             it(@"is ancestor of views 3, 4 and 5", ^{
                 [[theValue([vh.view2 tat_isAncestorOfView:vh.view3]) should] beYes];
                 [[theValue([vh.view2 tat_isAncestorOfView:vh.view4]) should] beYes];
@@ -59,7 +59,7 @@ describe(@"View Hierarchy", ^{
                 [[theValue([vh.view2 tat_isAncestorOfView:vh.view7]) should] beNo];
             });
         });
-        describe(@"view3", ^{
+        context(@"view3", ^{
             it(@"is ancestor of view 4", ^{
                 [[theValue([vh.view3 tat_isAncestorOfView:vh.view4]) should] beYes];
             });
@@ -71,7 +71,7 @@ describe(@"View Hierarchy", ^{
                 [[theValue([vh.view3 tat_isAncestorOfView:vh.view7]) should] beNo];
             });
         });
-        describe(@"view7", ^{
+        context(@"view7", ^{
             it(@"is not ancestor of any view", ^{
                 [[theValue([vh.view7 tat_isAncestorOfView:vh.view1]) should] beNo];
                 [[theValue([vh.view7 tat_isAncestorOfView:vh.view2]) should] beNo];
@@ -81,7 +81,7 @@ describe(@"View Hierarchy", ^{
                 [[theValue([vh.view7 tat_isAncestorOfView:vh.view6]) should] beNo];
             });
         });
-        describe(@"no view", ^{
+        context(@"no view", ^{
             it(@"is ancestor of view 7", ^{
                 [[theValue([vh.view1 tat_isAncestorOfView:vh.view7]) should] beNo];
                 [[theValue([vh.view2 tat_isAncestorOfView:vh.view7]) should] beNo];
@@ -94,7 +94,7 @@ describe(@"View Hierarchy", ^{
     });
     
     describe(@"closest ancestor shared with view", ^{
-        describe(@"view1", ^{
+        context(@"view1", ^{
             it(@"is the closest ancestor shared by views 2 and 6", ^{
                 [[[vh.view2 tat_closestAncestorSharedWithView:vh.view6] should] equal:vh.view1];
                 [[[vh.view6 tat_closestAncestorSharedWithView:vh.view2] should] equal:vh.view1];
@@ -119,7 +119,7 @@ describe(@"View Hierarchy", ^{
                 [[[vh.view2 tat_closestAncestorSharedWithView:vh.view1] should] equal:vh.view1];
             });
         });
-        describe(@"view2", ^{
+        context(@"view2", ^{
             it(@"is the closest ancestor shared by views 3 and 5", ^{
                 [[[vh.view3 tat_closestAncestorSharedWithView:vh.view5] should] equal:vh.view2];
                 [[[vh.view5 tat_closestAncestorSharedWithView:vh.view3] should] equal:vh.view2];
@@ -136,7 +136,7 @@ describe(@"View Hierarchy", ^{
                 [[[vh.view3 tat_closestAncestorSharedWithView:vh.view2] should] equal:vh.view2];
             });
         });
-        describe(@"view7", ^{
+        context(@"view7", ^{
             it(@"does not share any ancestor", ^{
                 [[[vh.view7 tat_closestAncestorSharedWithView:vh.view1] should] beNil];
                 [[[vh.view7 tat_closestAncestorSharedWithView:vh.view2] should] beNil];
