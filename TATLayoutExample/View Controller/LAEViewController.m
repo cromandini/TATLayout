e65bf088a9004a5e28ecfad14f13cfd430a18144
Claudio Romandini <cromandini@me.com>
2014-06-02 10:57:01 -0300

    Improve code legibility

--- TATLayoutExample/View Controller/LAEViewController.m
+++ TATLayoutExample/View Controller/LAEViewController.m
@@ -76,96 +76,93 @@
     [shuffleButton setTitle:@"Shuffle" forState:UIControlStateNormal];
     [shuffleButton setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
     
-    NSDictionary *views = NSDictionaryOfVariableBindings(albumArt, progressBar, timeElapsed, timeRemaining, songTitle, albumTitle, playButton, prevButton, nextButton, volumeSlider, repeatButton, shuffleButton);
+    NSDictionary *views = NSDictionaryOfVariableBindings(albumArt,
+                                                         progressBar,
+                                                         timeElapsed,
+                                                         timeRemaining,
+                                                         songTitle,
+                                                         albumTitle,
+                                                         playButton,
+                                                         prevButton,
+                                                         nextButton,
+                                                         volumeSlider,
+                                                         repeatButton,
+                                                         shuffleButton);
     
     [views enumerateKeysAndObjectsUsingBlock:^(id key, UIView *subview, BOOL *stop) {
         [view addSubview:subview];
         subview.translatesAutoresizingMaskIntoConstraints = NO;
     }];
     
-    NSDictionary *metrics = @{@"progressBarHeight": @15,
-                              @"playbackSpacing": @43,
+    NSDictionary *metrics = @{@"playbackSpacing": @43,
                               @"playbackButtonsSize": @44,
                               @"timeSpacing": @8,
-                              @"longPadding": @20,
                               @"shortPadding": @10,
+                              @"longPadding": @20,
                               @"progressToVolumeSpacing": @31,
-                              @"volumeMaxWidthPhone": @440,
-                              @"volumeMultiplierPad": @(3.0 / 4),
+                              @"volumeMultiplier": @(3.0 / 4),
                               @"highPriority": @751,
                               @"lowPriority": @251};
     
-    NSArray *formats = @[@"albumArt.height == albumArt.width",
-                         @"albumArt.top == superview.top @highPriority",
-                         @"albumArt.centerX == superview.centerX",
-                         @"albumArt.width == superview.width @252",
-                         
-                         @"progressBar.height == progressBarHeight",
-                         @"progressBar.leading == volumeSlider.leading + progressToVolumeSpacing",
-                         @"progressBar.trailing == volumeSlider.trailing - progressToVolumeSpacing",
-                         
-                         @"timeElapsed.centerY == progressBar.centerY",
-                         @"timeElapsed.trailing == progressBar.leading - timeSpacing",
+    NSArray *formats = @[@"albumArt.height       == albumArt.width",
+                         @"albumArt.top          == superview.top @highPriority",
+                         @"albumArt.centerX      == superview.centerX",
+                         @"albumArt.width        == superview.width @252",
+                         @"progressBar.height    == 15",
+                         @"progressBar.leading   == volumeSlider.leading + progressToVolumeSpacing",
+                         @"progressBar.trailing  == volumeSlider.trailing - progressToVolumeSpacing",
+                         @"timeElapsed.centerY   == progressBar.centerY",
+                         @"timeElapsed.trailing  == progressBar.leading - timeSpacing",
                          @"timeRemaining.centerY == progressBar.centerY",
                          @"timeRemaining.leading == progressBar.trailing + timeSpacing",
-                         
-                         @"songTitle.leading == superview.leading + longPadding",
-                         @"songTitle.trailing == superview.trailing - longPadding",
-                         
-                         @"albumTitle.leading == superview.leading + longPadding",
-                         @"albumTitle.trailing == superview.trailing - longPadding",
-                         
-                         @"playButton.width == playbackButtonsSize",
-                         @"playButton.height == playbackButtonsSize",
-                         @"playButton.centerX == superview.centerX",
-                         @"playButton.bottom == volumeSlider.top + 3",
-                         
-                         @"prevButton.width == playbackButtonsSize",
-                         @"prevButton.height == playbackButtonsSize",
-                         @"prevButton.centerY == playButton.centerY",
-                         @"prevButton.trailing == playButton.leading - playbackSpacing",
-                         
-                         @"nextButton.width == playbackButtonsSize",
-                         @"nextButton.height == playbackButtonsSize",
-                         @"nextButton.centerY == playButton.centerY",
-                         @"nextButton.leading == playButton.trailing + playbackSpacing",
-                         
-                         @"volumeSlider.centerX == superview.centerX",
-                         @"volumeSlider.leading >= superview.leading + longPadding @highPriority",
+                         @"songTitle.leading     == superview.leading + longPadding",
+                         @"songTitle.trailing    == superview.trailing - longPadding",
+                         @"albumTitle.leading    == superview.leading + longPadding",
+                         @"albumTitle.trailing   == superview.trailing - longPadding",
+                         @"playButton.width      == playbackButtonsSize",
+                         @"playButton.height     == playbackButtonsSize",
+                         @"playButton.centerX    == superview.centerX",
+                         @"playButton.bottom     == volumeSlider.top + 3",
+                         @"prevButton.width      == playbackButtonsSize",
+                         @"prevButton.height     == playbackButtonsSize",
+                         @"prevButton.centerY    == playButton.centerY",
+                         @"prevButton.trailing   == playButton.leading - playbackSpacing",
+                         @"nextButton.width      == playbackButtonsSize",
+                         @"nextButton.height     == playbackButtonsSize",
+                         @"nextButton.centerY    == playButton.centerY",
+                         @"nextButton.leading    == playButton.trailing + playbackSpacing",
+                         @"volumeSlider.centerX  == superview.centerX",
+                         @"volumeSlider.leading  >= superview.leading + longPadding @highPriority",
                          @"volumeSlider.trailing >= superview.trailing - longPadding @highPriority"];
-    
     [NSLayoutConstraint tat_installConstraintsWithEquationFormats:formats metrics:metrics views:views];
     
     if (LAEDeviceIsIPHONE) {
-        formats = @[@"albumArt.bottom <= progressBar.top - shortPadding @highPriority",
-                    @"progressBar.bottom == songTitle.top - 8",
-                    @"songTitle.bottom == albumTitle.top - 2",
-                    @"albumTitle.bottom == playButton.top - 9",
-                    @"volumeSlider.bottom == superview.bottom - 38",
-                    @"volumeSlider.width <= volumeMaxWidthPhone",
-                    @"repeatButton.leading == volumeSlider.leading - shortPadding",
-                    @"repeatButton.bottom == superview.bottom - 2",
+        formats = @[@"albumArt.bottom        <= progressBar.top - shortPadding @highPriority",
+                    @"progressBar.bottom     == songTitle.top - 8",
+                    @"songTitle.bottom       == albumTitle.top - 2",
+                    @"albumTitle.bottom      == playButton.top - 9",
+                    @"volumeSlider.bottom    == superview.bottom - 38",
+                    @"volumeSlider.width     <= 440",
+                    @"repeatButton.leading   == volumeSlider.leading - shortPadding",
+                    @"repeatButton.bottom    == superview.bottom - 2",
                     @"shuffleButton.trailing == volumeSlider.trailing + shortPadding",
-                    @"shuffleButton.bottom == repeatButton.bottom"];
-        
-        [NSLayoutConstraint tat_installConstraintsWithEquationFormats:formats metrics:metrics views:views];
+                    @"shuffleButton.bottom   == repeatButton.bottom"];
     } else {
-        formats = @[@"songTitle.top == superview.top + 16",
-                    @"albumTitle.top == songTitle.bottom + 4",
-                    @"albumArt.top == albumTitle.bottom + shortPadding",
-                    @"albumArt.bottom <= progressBar.top - 16",
-                    @"progressBar.bottom == playButton.top - 12",
-                    @"volumeSlider.width <= albumArt.width * volumeMultiplierPad",
-                    @"volumeSlider.centerY == repeatButton.centerY",
-                    @"repeatButton.leading == albumArt.leading @lowPriority",
-                    @"repeatButton.leading >= superview.leading + longPadding",
-                    @"repeatButton.bottom == superview.bottom - shortPadding",
+        formats = @[@"songTitle.top          == superview.top + 16",
+                    @"albumTitle.top         == songTitle.bottom + 4",
+                    @"albumArt.top           == albumTitle.bottom + shortPadding",
+                    @"albumArt.bottom        <= progressBar.top - 16",
+                    @"progressBar.bottom     == playButton.top - 12",
+                    @"volumeSlider.width     <= albumArt.width * volumeMultiplier",
+                    @"volumeSlider.centerY   == repeatButton.centerY",
+                    @"repeatButton.leading   == albumArt.leading @lowPriority",
+                    @"repeatButton.leading   >= superview.leading + longPadding",
+                    @"repeatButton.bottom    == superview.bottom - shortPadding",
                     @"shuffleButton.trailing == albumArt.trailing @lowPriority",
                     @"shuffleButton.trailing <= superview.trailing - longPadding",
-                    @"shuffleButton.centerY == repeatButton.centerY"];
-        
-        [NSLayoutConstraint tat_installConstraintsWithEquationFormats:formats metrics:metrics views:views];
+                    @"shuffleButton.centerY  == repeatButton.centerY"];
     }
+    [NSLayoutConstraint tat_installConstraintsWithEquationFormats:formats metrics:metrics views:views];
     
     self.songTitle = songTitle;
     self.albumTitle = albumTitle;
@@ -195,8 +192,8 @@
 - (NSLayoutConstraint *)iPhoneLandscapeConstraint
 {
     if (!_iPhoneLandscapeConstraint) {
-        NSString *format = @"albumArt.top == superview.top + 14";
-        NSDictionary *views = @{@"albumArt": self.albumArt};
+        NSString *format = @"art.top == superview.top + 14";
+        NSDictionary *views = @{@"art": self.albumArt};
         _iPhoneLandscapeConstraint = [NSLayoutConstraint tat_constraintWithEquationFormat:format metrics:nil views:views];
     }
     return _iPhoneLandscapeConstraint;
